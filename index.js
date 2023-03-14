1cf58109f80611f319ec44d420657ab8585cdacc
reeganbenny <reeganbenny@gmail.com>
2023-03-14 23:12:19 +0530

    Changes for profile upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
@@ -32,8 +31,8 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
